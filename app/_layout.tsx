a64cf139ff84cee188fe0162d71b462cebfecca0
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-06-26 21:58:24 +0300

    Enhance RootLayout component by integrating Stack for improved navigation structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
