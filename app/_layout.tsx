40017aad8e0fb3d8861e4c18294d97e9166d65d6
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-06-26 21:55:55 +0300

    Refactor RootLayout component to remove unused code and streamline rendering

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,5 @@
-
-import { StyleSheet, Text, View } from 'react-native';
-import { Slot } from 'expo-router';
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
- 
-
-  return (
-    <View style={styles.container}>
-      <Text>Root Layout</Text>
-    </View>
-  );
+  return <Slot />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
