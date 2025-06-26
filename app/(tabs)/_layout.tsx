a471f62b58c01e8076c5f08a8f8fd919d705c3c7
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-06-27 00:12:09 +0300

    update navigation: change profile link to home and enhance tab layout with custom icons for improved user experience

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs, Redirect } from "expo-router";
+
+import { icons } from "@/constants";
+
+const TabIcon = ({ color, icon, name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
-  )
-}
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{ title: "Home" }}
+          headerShown={false}
+          tabBarIcon={({ color, focused }) => (
+            <TabIcon
+              icon={icons.home}
+              name="Home"
+              color={color}
+              focused={focused}
+            />
+          )}
+        />
+      </Tabs>
+    </>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
