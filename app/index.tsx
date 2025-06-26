ebf0cb542c4b33f4477a014a6d8cef0aeb1a25b9
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-06-26 22:12:43 +0300

    Update index component to enhance UI with status bar and navigation link to profile page

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 const index = () => {
   return (
-    <View >
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Text>Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default index;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
