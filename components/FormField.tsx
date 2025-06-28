55f092d40262518c9e64cfbdc0b4acb00e3d5156
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-06-28 19:09:21 +0300

    bug fix; add focus state to FormField component: implement visual feedback for input focus with dynamic border styling

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -27,12 +27,17 @@ const FormField = ({
   ...props
 }: FormFieldProps) => {
   const [showPassword, setShowPassword] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
 
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
 
-      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
+      <View
+        className={`border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl items-center flex-row ${
+          isFocused ? "border-secondary" : ""
+        }`}
+      >
         <TextInput
           className="flex-1 text-white font-psemibold text-base"
           value={value}
@@ -40,6 +45,8 @@ const FormField = ({
           placeholder={placeholder}
           placeholderTextColor="#7b7b8b"
           secureTextEntry={title === "Password" && !showPassword}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
           {...props}
         />
 
