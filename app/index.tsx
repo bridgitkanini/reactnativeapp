3cf4d7dfdd3eda4000ddba6fc73083a479f2d147
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-06-28 17:58:00 +0300

    refactor index component: replace Link with Redirect for navigation and adjust button functionality to route to sign-in page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Image, ScrollView, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "@/constants";
@@ -19,7 +19,7 @@ const index = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85%] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -50,7 +50,7 @@ const index = () => {
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => {}}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
