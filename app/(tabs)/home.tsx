44230e55c7d42caacee807e120a8d5001f40cf97
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-04 19:43:06 +0300

    enhance Home component: add header with welcome message and logo, update FlatList item styling, and apply background color to SafeAreaView

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,14 +1,40 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { images } from "@/constants";
+
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-primary">
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
         keyExtractor={(item) => `${item.id}`}
-        renderItem={({ item }) => <Text className="text-3xl">{item.id}</Text>}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-36">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome back
+                </Text>
+                <Text className="font-psemibold text-2xl text-white">
+                  Admin00
+                </Text>
+              </View>
+              <View className="mt=1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+            
+          </View>
+        )}
       />
     </SafeAreaView>
   );
