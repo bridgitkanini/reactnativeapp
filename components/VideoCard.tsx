38cab39856ae8cf81338068841d0ef675c51f668
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-05 02:02:44 +0300

    bug fix; refactor VideoCard component: reorganize layout to improve readability and maintain consistency in rendering author information and menu icon

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -30,29 +30,26 @@ const VideoCard = ({
               className="w-full h-full rounded-lg"
               resizeMode="cover"
             />
+          </View>
 
-            <View className="justify-center flex-1 ml-3 gap-y-1">
-              <Text
-                className="text-white font-psemibold text-sm"
-                numberOfLines={1}
-              >
-                {title}
-              </Text>
-              <Text
-                className="text-gray-100 font-pregular text-xs"
-                numberOfLines={1}
-              >
-                {username}
-              </Text>
-            </View>
+          <View className="justify-center flex-1 ml-3 gap-y-1">
+            <Text
+              className="text-white font-psemibold text-sm"
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
+            <Text
+              className="text-gray-100 font-pregular text-xs"
+              numberOfLines={1}
+            >
+              {username}
+            </Text>
           </View>
-          <View className="pt-2">
-            <Image
-              source={icons.menu}
-              className="w-5 h-5"
-              resizeMode="contain"
-            />
-          </View>
+        </View>
+
+        <View className="pt-2">
+          <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
         </View>
       </View>
     </View>
