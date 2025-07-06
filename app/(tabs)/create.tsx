76f4379aa1b08c74cd3dde38a8a5c519170fa728
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-06 22:22:12 +0300

    implement video upload screen in Create component: add form for video title input, integrate video upload functionality with visual feedback, and enhance layout with SafeAreaView and ScrollView for better user experience

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
+import FormField from "@/components/FormField";
+import { VideoView, useVideoPlayer } from "expo-video";
+
+import { icons } from "@/constants";
+
+/**
+ * The Create component is a screen that allows users to upload videos.
+ * It renders a SafeAreaView with a ScrollView inside. The ScrollView
+ * contains a Text component with the title "Upload Video" and a FormField
+ * component for entering the title of the video. The FormField component
+ * is initially empty and doesn't do anything when the user types something
+ * in it.
+ */
 const Create = () => {
-  return (
-    <View>
-      <Text>Create</Text>
-    </View>
-  )
-}
+  const [uploading, setUploading] = useState(false);
 
-export default Create
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
+
+  return (
+    <SafeAreaView className="bg-primary h-full ">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
+        <FormField
+          title="Video Title"
+          value={form.title}
+          placeholder="Give your video a catchy title..."
+          handleChangetext={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-10"
+        />
+
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Upload Video
+          </Text>
+          <TouchableOpacity>
+            {form.video ? (
+              <VideoView
+                player={player}
+                style={{
+                  width: "100%",
+                  height: 240, // h-60
+                  borderRadius: 12,
+                  marginTop: 12,
+                }}
+                allowsFullscreen
+                allowsPictureInPicture
+              />
+            ) : (
+              <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
+                <View className="w-14 h-14 border border-dashed border-x-secondary-100 justify-center items-center">
+                  <Image source={icons.upload} resizeMode="contain" className="w-1/2 h-1/2"  />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Create;
