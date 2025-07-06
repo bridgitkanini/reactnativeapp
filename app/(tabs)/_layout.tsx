b19f2212ca7b167531ac8646c4b80377a214449a
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-07 02:22:08 +0300

    remove Bookmark screen component and comment out related Tabs.Screen in layout; streamline tab navigation by eliminating unused screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -76,7 +76,7 @@ const TabsLayout = () => {
             ),
           }}
         />
-        <Tabs.Screen
+        {/* <Tabs.Screen
           name="bookmark"
           options={{
             title: "Bookmark",
@@ -96,7 +96,7 @@ const TabsLayout = () => {
               />
             ),
           }}
-        />
+        /> */}
         <Tabs.Screen
           name="create"
           options={{
